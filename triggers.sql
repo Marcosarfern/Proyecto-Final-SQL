ea2cdd0f12e3e8e6d8fbcca2eacdd18038459677
Marcos Ariel Fernandez <mcsfernand@gmail.com>
2024-08-01 22:17:57 -0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -15,13 +15,3 @@ END //
 
 DELIMITER ;
 
-
---Insertar un Comentario
---Para probar el trigger, insertemos un nuevo comentario en la tabla COMENTARIOS. Asegúrate de tener al menos un cliente y una solicitud existente para usar en este ejemplo:
-INSERT INTO COMENTARIOS (ID_SOLICITUD, ID_CLIENTE, CALIFICACION, COMENTARIO, FECHA)
-VALUES (1, 1, 2, 'El servicio fue muy deficiente.', NOW());
-
-
---Verificar la Inserción en MALAS_CALIFICACIONES
---Después de realizar la inserción, puedes verificar si la mala calificación se registró correctamente en la tabla MALAS_CALIFICACIONES:
-SELECT * FROM MALAS_CALIFICACIONES;
