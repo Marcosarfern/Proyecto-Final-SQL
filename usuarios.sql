f4332b5eaed4c0e9e1394e60f9c91765eac47d53
Marcos Ariel Fernandez <mcsfernand@gmail.com>
2024-08-30 23:04:39 -0300

    Update usuarios.sql

--- usuarios.sql
+++ usuarios.sql
@@ -1,10 +1,10 @@
 --USUARIO ADMIN --
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'TuContraseñaSegura';
+CREATE USER 'usuario_admin'@'localhost' IDENTIFIED BY 'Admin1';
 GRANT ALL PRIVILEGES ON proyecto_appservcios.* TO 'admin'@'localhost';
 FLUSH PRIVILEGES;
 
 --USUARIO FINANZAS --
-CREATE USER 'usuario_contable1'@'localhost' IDENTIFIED BY 'contraseña1';
+CREATE USER 'usuario_contable'@'localhost' IDENTIFIED BY 'Finanzas1';
 GRANT SELECT ON proyecto_appservcios.FACTURAS TO 'usuario_contable1'@'localhost';
 GRANT SELECT ON proyecto_appservcios.EMPLEADO TO 'usuario_contable1'@'localhost';
 GRANT SELECT ON proyecto_appservcios.CLIENTE TO 'usuario_contable1'@'localhost';
@@ -14,3 +14,7 @@ GRANT SELECT ON proyecto_appservcios.MEDIODEPAGO TO 'usuario_contable1'@'localho
 FLUSH PRIVILEGES;
 
 --USUARIO MARKETING --
+CREATE USER 'usuario_marketing'@'localhost' IDENTIFIED BY 'Marketing1';
+GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_appservcios.CLIENTE TO 'usuario_marketing'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON proyecto_appservcios.NOTIFICACIONES TO 'usuario_marketing'@'localhost';
+FLUSH PRIVILEGES;
